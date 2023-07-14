e0b96574fc4595dbf798c94d602665b0775956bb
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-07-15 01:27:24 +0200

    Fix exports

--- src/mod.ts
+++ src/mod.ts
@@ -1,10 +1,69 @@
-export * from './Column';
-export * from './Database';
-export * from './Datatype';
-export * from './Expr';
-export * from './Operation';
-export * from './Populate';
-export * from './Random';
-export * from './Table';
-export * from './TableQuery.types';
-export * from './utils/types';
+export { Column, type DefaultValueBase, type IColumn, type IColumnAny } from './Column';
+export { Database } from './Database';
+export { Datatype, type DatatypeAny, type SqliteDatatype } from './Datatype';
+export {
+  Expr,
+  type ExprParser,
+  type IExpr,
+  type IExprAny,
+  type IExprAstParam,
+  type IExprInternal,
+  type IExprUnknow,
+  type JsonMode,
+} from './Expr';
+export type {
+  ICreateTableOperation,
+  IDeleteOperation,
+  IInsertOperation,
+  IListTablesOperation,
+  IOperation,
+  IOperationKind,
+  IOperationResult,
+  IPragmaOperation,
+  IPragmaSetOperation,
+  IQueryOperation,
+  IUpdateOperation,
+} from './Operation';
+export {} from './Populate';
+export { Random } from './Random';
+export { Table, type DeleteOptions, type ITable, type ITableSchemaOptions, type UpdateOptions } from './Table';
+export type {
+  AllColsFn,
+  AllColsFnOrRes,
+  ColsFn,
+  ColsFnOrRes,
+  ColsRefInnerJoined,
+  ColsRefLeftJoined,
+  FilterEqualCols,
+  IPaginateConfig,
+  ITableQuery,
+  ITableQueryInternal,
+  ITableQueryInternalBase,
+  ITableQueryState,
+  ITakeConfig,
+  OrderByItem,
+  OrderingTerms,
+  SelectFn,
+} from './TableQuery.types';
+export type {
+  AnyRecord,
+  ColumnInputValue,
+  ColumnOutputValue,
+  ColumnToExpr,
+  ColumnsBase,
+  ColumnsToExprRecord,
+  ColumnsToInput,
+  ExprFnFromTable,
+  ExprRecord,
+  ExprRecordNested,
+  ExprRecordOutput,
+  ExprRecord_MakeNullable,
+  ExprResult,
+  ExprResultFrom,
+  ExprsNullables,
+  ExtractDefinedKeys,
+  ExtractUndefinedKeys,
+  MarkUndefinedOptional,
+  Prettify,
+  QueryColumnValuePrimitive,
+} from './utils/types';
